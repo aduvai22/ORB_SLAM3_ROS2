a54fa0f5054e9ab997e0720c5be7b687e10aaeac
aduvai22 <adnanabdullah@ufl.edu>
2025-05-08 15:44:57 -0400

    launch

---
+++ launch/stereo_inertial_launch.py
@@ -0,0 +1,23 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='orbslam3',
+            executable='stereo-inertial',
+            name='orbslam3_stereo_inertial',
+            output='screen',
+            remappings=[
+                ('/cam0/image_raw', '/a15/camera/left/image_raw'),
+                ('/cam1/image_raw', '/a15/camera/right/image_raw'),
+                ('/imu0', '/a15/imu/data')
+            ],
+            arguments=[
+                '/root/aqua_ws/src/orbslam3_ros2/vocabulary/ORBvoc.txt',
+                '/root/aqua_ws/src/orbslam3_ros2/config/stereo-inertial/TUM-VI.yaml',
+                'false',
+                'false'
+            ]
+        )
+    ])
